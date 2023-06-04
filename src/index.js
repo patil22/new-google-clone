2377fc04485db5a19b6ad808f9cba479fee5aa37
patil22 <113407263+patil22@users.noreply.github.com>
2023-06-03 23:22:38 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,19 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import reducer,{initialState} from './Pages/Reducer'
-
+import reducer, { initialState } from './Pages/Reducer';
 import StateProvider from './Pages/StateProvider';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+ReactDOM.render(
   <React.StrictMode>
     <StateProvider initialState={initialState} reducer={reducer}>
-    <App />
+      <App />
     </StateProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
